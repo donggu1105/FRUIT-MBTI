a8ca8b32e14bae2aa1e75aefedd6f57ad50bdec1
donggu1105 <kbh11051@gmail.com>
2022-04-06 12:22:23 +0900

    header footer

---
+++ js/objects.js
@@ -0,0 +1,37 @@
+
+var num = 1;
+var q = {
+    1: {"title": "문제 1번", "type": "EI", "A": "E", "B": "I", "C": "T", "D": "F"},
+    2: {"title": "문제 2번", "type": "EI", "A": "E", "B": "I"},
+    3: {"title": "문제 3번", "type": "EI", "A": "E", "B": "I"},
+    4: {"title": "문제 4번", "type": "SN", "A": "S", "B": "N"},
+    5: {"title": "문제 5번", "type": "SN", "A": "S", "B": "N"},
+    6: {"title": "문제 6번", "type": "SN", "A": "S", "B": "N"},
+    7: {"title": "문제 7번", "type": "TF", "A": "T", "B": "F"},
+    8: {"title": "문제 8번", "type": "TF", "A": "T", "B": "F"},
+    9: {"title": "문제 9번", "type": "TF", "A": "T", "B": "F"},
+    10: {"title": "문제 10번", "type": "JP", "A": "J", "B": "P"},
+    11: {"title": "문제 11번", "type": "JP", "A": "J", "B": "P"},
+    12: {"title": "문제 12번", "type": "JP", "A": "J", "B": "P"}
+}
+
+var result = {
+    "ISTJ" : {"animal": "하마", "explain": "하마 설명", "img": "lion.jpeg"},
+    "ISFJ" : {"animal": "하마", "explain": "하마 설명", "img": "lion.jpeg"},
+    "INFJ" : {"animal": "하마", "explain": "하마 설명", "img": "lion.jpeg"},
+    "INTJ" : {"animal": "하마", "explain": "하마 설명", "img": "lion.jpeg"},
+    "ISTP" : {"animal": "하마", "explain": "하마 설명", "img": "lion.jpeg"},
+    "ISFP" : {"animal": "하마", "explain": "하마 설명", "img": "lion.jpeg"},
+    "INFP" : {"animal": "하마", "explain": "하마 설명", "img": "lion.jpeg"},
+    "INTP" : {"animal": "하마", "explain": "하마 설명", "img": "lion.jpeg"},
+    "ESTP" : {"animal": "하마", "explain": "하마 설명", "img": "lion.jpeg"},
+    "ESFP" : {"animal": "하마", "explain": "하마 설명", "img": "lion.jpeg"},
+    "ENFP" : {"animal": "사자", "explain": "사자 설명", "img": "lion.jpeg"},
+    "ENTP" : {"animal": "사자", "explain": "사자 설명", "img": "lion.jpeg"},
+    "ESTJ" : {"animal": "사자", "explain": "사자 설명", "img": "lion.jpeg"},
+    "ESFJ" : {"animal": "사자", "explain": "사자 설명", "img": "lion.jpeg"},
+    "ENFJ" : {"animal": "사자", "explain": "사자 설명", "img": "lion.jpeg"},
+    "ENTJ" : {"animal": "사자", "explain": "사자 설명", "img": "lion.jpeg"},
+
+
+}
