fee45c2cc766d7de9e2412e3acfcc2a0e87013b1
donggu1105 <kbh11051@gmail.com>
2022-04-06 22:28:23 +0900

    메인페이지 완료

---
+++ start/js/start.js
@@ -0,0 +1,20 @@
+const main = document.querySelector("#main");
+const qna = document.querySelector("#qna");
+
+function begin() {
+    main.style.WebkitAnimation = "fadeOut 1s";
+    main.style.animation = "fadeOut 1s";
+
+    setTimeout(() => {
+
+        qna.style.WebkitAnimation = "fadeIn 1s";
+        qna.style.animation = "fadeIn 1s";
+        setTimeout(() => {
+            main.style.display = "none"
+            qna.style.display = "block";
+
+        }, 450)
+    }, 450);
+
+
+}
