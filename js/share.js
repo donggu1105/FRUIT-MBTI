bc575871f1c7027c107d13af554cbce798739ff1
donggu1105 <kbh11051@gmail.com>
2022-04-09 06:22:17 +0900

    kakako

--- js/share.js
+++ js/share.js
@@ -1,3 +1,40 @@
+const url = "https://stjustdo.netlify.app/";
+
+
+function setShare() {
+    var resultImg = document.querySelector("#resultImg")
+    var resultAlt = resultImg.firstElementChild.alt;
+
+    const shareTitle = "단톡방 MBTI 유형"
+    const shareDesc = infoList[resultAlt].name;
+    const shareImage = url + "img/image-" + resultAlt + ".png";
+    const shareURL = url + "page.result-" + resultAlt + '.html';
+
+    Kakao.Link.sendDefault({
+        objectType: 'feed',
+        content: {
+            title: shareTitle,
+            description: shareDesc,
+            imageUrl:
+                shareImage,
+            link: {
+                mobileWebUrl: shareURL,
+                webUrl: shareURL
+            },
+        },
+        buttons: [
+            {
+                title: '결과 확인하기',
+                link: {
+                    mobileWebUrl: shareURL,
+                    webUrl: shareURL,
+                },
+            },
+        ],
+    })
+}
+
+
 
 
 
