edf1971919fa3d7fba675f8fe0139f5e042a92d8
donggu1105 <kbh11051@gmail.com>
2022-06-06 16:11:43 +0900

    js

--- js/start.js
+++ js/start.js
@@ -143,6 +143,7 @@ function calResult() {
 function setResult() {
 
     let mbti = calResult();
+    mbti = "ISTJ";
     // 이름
     const resultName = document.querySelector(".resultname");
     resultName.innerHTML = mbtiInfo[mbti]["nickName"];
@@ -154,35 +155,35 @@ function setResult() {
     resultImg.alt = mbti;
     resultImg.classList.add('img-fluid');
     imgDiv.appendChild(resultImg);
+
+    // nickName : '<멜론>',
+    //     desc: '수박과는 비슷하면서도 달라! 나만의 달콤함으로 승부한다!',
+    //     content: ["전화보다는 카톡! 전화는 즉각적으로 대응해야해서 좀 곤란해요!",
+    //     "매순간 카톡하긴 힘들어~ 정해진 시간에 카톡할게 나만의 원칙이 있다구요! (많이 관심가는상대면 예외)",
+    //     "바빠서 미처 읽씹한 상대한테는 꼭 미안하다고 말해요! 그게 매너라고 생각해요.",
+    //     "친구가 한 걱정을 들으면서 겉으론 끄덕끄덕하지만 속으로는 누가누가 좋은놈 나쁜놈인지 계산완료 끄읏~",
+    //     "친한 친구들이라면 용건만 간단히 해주세요! (아무 이유 없이 대체 전화를 왜 거는 거야?)"],
+    //     goodSynergyList : ['ENFP', 'ENTP', 'ISFP', 'INFP'],
+    //     badSynergyList : ['ENTJ', 'INFJ', 'ENTJ', 'ESTP'],
+    //     rareRate : 25,
+    //     keyWords : ['책임감', '원리원칙', '고지식', '팩폭러']
     // 한줄 설명
     const resultDesc = document.querySelector('.resultDesc');
     resultDesc.innerHTML = mbtiInfo[mbti]["desc"];
-
     // 콘텐트
     const resultContent = document.querySelector('.resultContent');
-    resultContent.innerHTML = mbtiInfo[mbti]["content"];
-
+    resultContent.innerHTML = mbtiInfo[mbti]["content"].join("<br>");
     // 희귀도
     const resultRareRate = document.querySelector('.resultRareRate');
     resultRareRate.innerHTML = mbtiInfo[mbti]["rareRate"];
-
     // goodSynergyList
     const resultGoodSynergy = document.querySelector('.resultGoodSynergy')
-    let goodSynergyList = mbtiInfo[mbti]["goodSynergyList"];
-    for (let i = 0; i < goodSynergyList.length; i++) {
-        resultGoodSynergy.innerHTML += goodSynergyList[i] + "<br>"
-    }
-
+    resultGoodSynergy.innerHTML  = mbtiInfo[mbti]["goodSynergyList"].join("<br>");
     // badSynergyList
     const resultBadSynergy = document.querySelector('.resultBadSynergy')
-    let badSynergyList = mbtiInfo[mbti]["badSynergyList"];
-    for (let i = 0; i < badSynergyList.length; i++) {
-        resultBadSynergy.innerHTML += badSynergyList[i] + "<br>"
-    }
-
+    resultBadSynergy.innerHTML = mbtiInfo[mbti]["badSynergyList"].join("<br>");
     // keyWords
-
-
-    mbtiInfo[mbti]["keyWords"];
+    const resultKeyWords = document.querySelector('.resultKeyWords')
+    resultKeyWords.innerHTML = mbtiInfo[mbti]["keyWords"].join("<br>");
 
 }
